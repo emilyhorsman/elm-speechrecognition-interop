39450ea3748921b1784e295db8c49d9ef6486795
Emily Horsman <me@emilyhorsman.com>
2016-11-19 19:08:48 -0500

    Publishing requirements

--- src/SpeechRecognitionInterop.elm
+++ src/SpeechRecognitionInterop.elm
@@ -1,18 +1,30 @@
 module SpeechRecognitionInterop exposing (Alternative, Result, Event)
 
+{-| Types for an interop between Elm and the HTML5 SpeechRecognition API.
 
+# Definition
+@docs Alternative, Result, Event
+-}
+
+
+{-| https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionAlternative
+-}
 type alias Alternative =
     { transcript : String
     , confidence : Float
     }
 
 
+{-| https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResult
+-}
 type alias Result =
     { isFinal : Bool
     , items : List Alternative
     }
 
 
+{-| https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionEvent
+-}
 type alias Event =
     { resultIndex : Int
     , results : List Result
